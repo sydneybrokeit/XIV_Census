7e5c6a8067fa046afe857ee6894eae6917949be4
hmschreck <harold@haroldmschreckengost.com>
2015-11-27 21:59:25 -0500

    Update
    Added labels to two graphs, fixed a stupid bug in my variables

--- process.R
+++ process.R
@@ -2,7 +2,7 @@ require(ggplot2); #for drawing
 require(scales); #for adjusting scales in charts
 
 #Set Filename Here
-playerSetName <- "FullSet.csv";
+playerSetName <- "PlayersTotal.csv";
 levelBreaks <- (0:12 * 5);
 levelBreaks_small <- (0:6*10);
 
@@ -35,7 +35,7 @@ gc_colors <- c("#FFFFFF", "#333333","#FF6666","#DDDD00");
 
 
 #Read playerSetName file (.csv file container data), omitting first two columns (ID and Name)
-playerData <- read.csv("FullSet.csv", colClasses = c("NULL", "NULL", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA));
+playerData <- read.csv(playerSetName, colClasses = c("NULL", "NULL", NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA));
 for(i in 5:27){
   colnames(playerData)[i] <- getPlayerClassName(colnames(playerData)[i])
   }
@@ -53,15 +53,15 @@ generateCharts <- function(pDataSet, location, appendText) {
     pDataSubset <- pDataSet[, c("realm", i)];
     pcName <- .simpleCap(i);
     saveLocation <- paste(location,"/",pcName,".png", sep="");
-    print(ggplot(pDataSubset, aes_string(x=i)) + geom_histogram(binwidth=5, origin=0.5) + geom_histogram(binwidth = 1, color="white", fill="white", origin=0.5) + scale_x_continuous(breaks=levelBreaks, name=paste(pcName, appendText)) + scale_y_continuous(labels=comma));
+    print(ggplot(pDataSubset, aes_string(x=i)) + geom_histogram(binwidth = 1, origin=0.5) + scale_x_continuous(breaks=levelBreaks, name=paste(pcName, appendText)) + scale_y_continuous(labels=comma));
     ggsave(saveLocation, width=11.2, height=6.06);
     saveLocation <- paste(location,"/faceted/",pcName,".png", sep="");
-    print(ggplot(pDataSubset, aes_string(x=i)) + geom_histogram(binwidth = 1, origin=0.5) + scale_x_continuous(breaks=levelBreaks_small, name=paste(pcName, appendText)) + facet_wrap(~realm) + theme(axis.text.x=element_text(angle=-90, vjust=0.5)) );
+   print(ggplot(pDataSubset, aes_string(x=i)) + geom_histogram(binwidth = 1, origin=0.5) + scale_x_continuous(breaks=levelBreaks_small, name=paste(pcName, appendText)) + facet_wrap(~realm) + theme(axis.text.x=element_text(angle=-90, vjust=0.5)) );
     ggsave(saveLocation, height=11, width=11, units="in");
   }
   #Generates Grand Company charts
   grandCompanies <- subset(pDataSet[4], grand_company != "");
-  ggplot(grandCompanies, aes(x=grand_company)) + geom_bar(stat="bin") + scale_x_discrete(name=paste("Grand Company", appendText));
+  ggplot(grandCompanies, aes(x=grand_company)) + geom_bar(stat="bin") + scale_x_discrete(name=paste("Grand Company", appendText)) + stat_bin(geom = "text", aes(label = paste((..count..)), vjust = 5));
   ggsave(paste(location, "/GC.png", sep=""));
   
   dir.create(paste(location, "/demographics/", sep=""));
@@ -71,9 +71,9 @@ generateCharts <- function(pDataSet, location, appendText) {
   popCount <- popCount[serverRanking];
   ggplot(pDataSet, aes(x=realm, fill=gender)) + geom_bar(stat="bin") + scale_x_discrete(name=paste("Server Population (with gender)",appendText), limits=names(popCount)) + theme(axis.text.x=element_text(angle=-90, vjust=0.5)) + scale_y_continuous(labels = comma);
   ggsave(paste(location, "/demographics/serverPopGender.png", sep=""), width=11.2, height=6.06);
-  ggplot(pDataSet, aes(x=realm, fill=grand_company)) + geom_bar(stat="bin") + scale_x_discrete(name=paste("GC Affiliation by Server", appendText), limits=names(popCount)) + theme(axis.text.x=element_text(angle=-90, vjust=0.5)) + scale_y_continuous(labels = comma) + scale_fill_manual(values=gc_colors)
+  ggplot(pDataSet, aes(x=realm, fill=grand_company)) + geom_bar(stat="bin") + scale_x_discrete(name=paste("GC Affiliation by Server", appendText), limits=names(popCount)) + theme(axis.text.x=element_text(angle=-90, vjust=0.5)) + scale_y_continuous(labels = comma) + scale_fill_manual(values=gc_colors);
   ggsave(paste(location, "/demographics/serverPopGC.png", sep=""), width=11.2, height=6.06);
-  ggplot(pDataSet, aes(x=race, fill=gender)) + geom_bar(position="dodge") + scale_x_discrete(name=paste("Races", appendText));
+  ggplot(pDataSet, aes(x=race, fill=gender)) + geom_bar(position="dodge") + scale_x_discrete(name=paste("Races", appendText))  + stat_bin(geom = "text", aes(label = paste((..count..)), vjust = -0.25), position=position_dodge(width=0.9)) + stat_bin(geom = "text", aes(label = paste(round((..count..)/sum(..count..)*100, 2), "%", sep=""), vjust = 2), position=position_dodge(width=0.9));
   ggsave(paste(location, "/demographics/race.png", sep=""), width=11.2, height=6.06);
   ggplot(pDataSet, aes(x=realm, fill=race)) + geom_bar(stat="bin") + scale_x_discrete(name=paste("Server Population (by race)",appendText), limits=names(popCount)) + theme(axis.text.x=element_text(angle=-90, vjust=0.5)) + scale_y_continuous(labels = comma);
   ggsave(paste(location, "/demographics/serverpopRace.png", sep=""), width=11.2, height=6.06);
